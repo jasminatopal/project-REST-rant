2c068aeb2d79d6881087939af8ddd636f7110113
unknown <jasmina_t05@outlook.com>
2023-03-27 20:55:53 -0700

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,30 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
+
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
+router.get('/', (req, res) => {
+  res.render('places/index', { 
+    places: places, })
+
+})
+
+
+
+router.post('/', (req, res) => {
+  req.body.pic = req.body.pic || 'http://placekitten.com/400/400'
+  req.body.city = req.body.city || 'Anytown'
+  req.body.state = req.body.state || 'USA'
+
+  places.push(req.body)
+  res.redirect('/places')
+})
+
+
+
 router.get('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
@@ -13,6 +37,15 @@ router.get('/:id', (req, res) => {
     res.render('places/show', { place: places[id], id })
   }
 })
+
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id) || !places[id]) {
+      res.render('error404')
+  }
+  res.render('places/edit', { place: places[id], id })
+})
+
 
 router.delete('/:id', (req, res) => {
   let id = Number(req.params.id)
@@ -29,31 +62,10 @@ router.delete('/:id', (req, res) => {
 })
 
 
-router.get('/', (req, res) => {
-  res.render('places/index', { 
-    places: places, })
 
-})
 
-router.get('/new', (req, res) => {
-  res.render('places/new')
-})
 
-router.post('/', (req, res) => {
-  console.log(req.body)
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
-  }
-  if (!req.body.city) {
-    req.body.city = 'Anytown'
-  }
-  if (!req.body.state) {
-    req.body.state = 'USA'
-  }
-  places.push(req.body)
-  res.redirect('/places')
-})
+
 
 
 module.exports = router
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -1,40 +1,67 @@
 const React = require('react')
-const Def = require('../default')
+const Default = require('../default')
 
-function new_form() {
+function New() {
     return (
-        <Def>
+        <Default>
             <main>
                 <h1>Add a New Place</h1>
-                <form method="POST" action="/places">
-                    <div className="form-group">
-                        <label htmlFor="name">Place Name</label>
-                        <input className="form-control" id="name" name="name" required />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="pic">Photo</label>
-                        <input className="form-control" type="url" id="pic" name="pic" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="city">City</label>
-                        <input className="form-control" id="city" name="city" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="state">State</label>
-                        <input className="form-control" id="state" name="state" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="cuisines">Cuisine</label>
-                        <input className="form-control" id="cuisines" name="cuisines" required />
+                <br/>
+                <form method='POST' action='/places'>
+                    <div className='row'>
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="name">Name</label>
+                            <input
+                                className="form-control" 
+                                id="name" 
+                                name="name" 
+                                type="text"
+                                required />
+                        </div>
+
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="pic">Picture</label>
+                            <input
+                                className="form-control" 
+                                id="pic" 
+                                name="pic" 
+                                type="url" />
+                        </div>
+
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="city">City</label>
+                            <input
+                                className="form-control" 
+                                id="city" 
+                                name="city" 
+                                type="text" />
+                        </div>
+
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="state">State</label>
+                            <input
+                                className="form-control" 
+                                id="state" 
+                                name="state" 
+                                type="text" />
+                        </div>
+
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="cuisines">Cuisines</label>
+                            <input
+                                className="form-control" 
+                                id="cuisines" 
+                                name="cuisines" 
+                                type="text" 
+                                required />
+                        </div>
+                        <br/>
                     </div>
                     <input className="btn btn-primary" type="submit" value="Add Place" />
                 </form>
-
             </main>
-        </Def>
+        </Default>
     )
-
 }
 
-module.exports = new_form
-
+module.exports = New
