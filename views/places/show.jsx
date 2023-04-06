8e951fc2acac1d3077d5943f5af9f9848aed40d9
unknown <jasmina_t05@outlook.com>
2023-04-05 22:33:53 -0700

    added commentary

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,54 +1,79 @@
 const React = require('react')
-const Def = require('../default')
+const Default = require('../default')
 
-function show(data) {
-    let message = ''                 
-    if (data.message) {
-      message = (
-        <h4 className="alert-danger">
-          {data.message}
-        </h4>
-      )
+function Show(data) {
+    let comments = (
+        <h6 className='inactive'>
+            No comments yet! 😭
+        </h6>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map((c) => {
+            return (
+                <div className='border'>
+                    <h4 className='rant'>{c.rant ? 'Rant!!! 🤬' : 'Rave!!! 😁'}</h4>
+                    <blockquote className="blockquote">{c.content}</blockquote>
+                    <figcaption className="blockquote-footer">{c.author}</figcaption>
+                    <h4>{ "⭐".repeat(c.stars)}</h4>
+                </div>
+            )
+        })
     }
+
     return (
-        <Def>
-            <main>
-                <div>
-                    <h1>{data.place.name}</h1>
-                    {message}
-                    <img src="https://www.omnihotels.com/-/media/images/hotels/ausctr/restaurants/presidents-house/ausctr-omni-austin-hotel-downtown-presidents-house-coffee-shop.jpg?w=992" alt="cafe" width="600" height="400" />
-                    <h3>
-                        Located in {data.place.city}, {data.place.state}
-                    </h3>
-                    <h3>
-                        {data.place.showEstablished()}
-                    </h3>
-                    <h4>
-                        Serving {data.place.cuisines}
-                    </h4>
-                    <h5>Rating</h5>
-                    <p>Not Rated</p>
-                    <h5>Comments</h5>
-                    <p>No comments yet</p>
+        <Default>
+            <main className="px-5 py-4">
+                <div className="row">
+                    <div className="col-sm">
+                        <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">✏️ Edit</a>    
+                    </div>
+                    <div className="col-sm">
+                        <h1>{data.place.name}</h1>
+                    </div>
+                    <div className="col-sm">
+                        <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}> 
+                            <button type="submit" className="btn btn-danger">
+                                🗑️ Delete
+                            </button>
+                        </form>
+                    </div>
+                </div>
 
-                    <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-                        Edit
-                    </a>
+                <div className="row">
+                    <div className="col-sm">
+                        <img src={data.place.pic} alt={data.place.name} className="img-max" />                    
+                    </div>
 
-                    <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-                        <button type="submit" className="btn btn-danger">
-                            Delete
-                        </button>
-                    </form>
+                    <div className="col-sm">
+                        <div className="row">
+                            <h3>Description</h3>
+                            <p className="text-center bigger-font">
+                                {data.place.showEstablished()}
+                                <br />
+                            </p>
+                        </div>
 
+                        <div className="row">
+                            <h3>Ratings</h3>
+                        </div>
 
-
+                        <div className="row">
+                            <div className="col-sm-4"></div>
+                            <div className="col-sm-4">
+                                <h3>Comments</h3>
+                            </div>
+                            <div className="col-sm-4">
+                                <a href={`/places/${data.place.id}/comment`} className="btn btn-primary">
+                                    ➕ Add your review!
+                                </a>
+                            </div>
+                            {comments}
+                        </div>                
+                    </div>
                 </div>
             </main>
-        </Def>
+        </Default>
     )
 }
 
-
-
-module.exports = show
+module.exports = Show
